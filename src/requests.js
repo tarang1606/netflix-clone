43bee1d248b0677af8c8b53d67120a04aa9735db
tarang1606 <tarangkalaria55@gmail.com>
2021-05-31 21:06:14 +0530

    build 1

--- src/requests.js
+++ src/requests.js
@@ -1,12 +1,12 @@
 const API_KEY = "fbbb70ebbc541fdfc9ea1731d01ee1b1";
 
 const requests = {
-// fetchTrending : `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-// fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-// fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-// fetchActionMovies: `/discover/moview`
+	// fetchTrending : `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	// fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+	// fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	// fetchActionMovies: `/discover/moview`
 
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
 	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
 	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
 	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
